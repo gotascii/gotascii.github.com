76c733329085cccde85648731062e00fda216c41
Justin Marney <gotascii@gmail.com>
2010-04-11 20:52:13 -0400

    remove disqus from master

--- javascripts/disqus.js
+++
@@ -1,8 +0,0 @@
-var disqus_developer = 1;
-$(function () {
-  var dsq = document.createElement('script');
-  dsq.type = 'text/javascript';
-  dsq.async = true;
-  dsq.src = 'http://sorescode.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);  
-});
