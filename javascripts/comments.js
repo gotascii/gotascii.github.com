f0f52adee03a8eef6e2804247d077355125022d7
Justin Marney <gotascii@gmail.com>
2010-04-13 11:29:05 -0400

    added template jquery snippets

--- javascripts/comments.js
+++ javascripts/comments.js
@@ -1,8 +1,11 @@
 $(function () {
   $.getJSON("http://localhost:4567/comments.js?callback=?", function(data) {
-    $("#comments #no-comments-heading").replaceWith();
-    // $.each(data, function (idx, comment) {
-    //   $("#comments").append("<p>" + comment.body + "</p>");
-    // });
+    if (data.length > 0) {
+      var show_comment = tmpl("comment_tmpl");
+      var comments = $.map(data, function (comment, idx) {
+        return show_comment(comment);
+      }).join("\n");
+      $("#comments #no-comments-heading").replaceWith("<ol class=\"comment_list\">" + comments + "</ol>");
+    }
   });
 });
